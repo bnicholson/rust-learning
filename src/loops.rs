4f58fbbf0156dd3f5c98989d66a50d3ae7277a4c
B. Nicholson <b.nicholson@niceng.com>
2020-10-12 15:35:01 -0500

    Loops were easy.

--- src/loops.rs
+++ src/loops.rs
@@ -13,4 +13,28 @@ pub fn run() {
         }
     }
 
+    // while (fizzbuzz)
+    count = 0;
+    while count < 100 {
+        if count % 15 == 0 {
+            println!("Fizzbuzz {}",count);
+        } else if count % 3 == 0{
+            println!("Fizz {}",count);
+        } else if count % 5 == 0 {
+            println!("Buzz {}",count);
+
+        }
+        count += 1;
+    }
+
+    // now do a range for
+    for x in 1..99 {
+        if x % 15 == 0 {
+            println!("Fizzbuzz {}",x);
+        } else if x % 3 == 0{
+            println!("Fizz {}",x);
+        } else if x % 5 == 0 {
+            println!("Buzz {}",x);
+        }
+    }  
 }
